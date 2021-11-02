87c19871e872505964d83390f23844c509c10861
Collins Kanyiri <collins.kanyiri@student.moringaschool.com>
2021-11-02 10:40:32 +0300

    add profile,home,signup

--- neighbor/views.py
+++ neighbor/views.py
@@ -5,3 +5,32 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
+@login_required(login_url='/login/')
+def home(request):
+    current_user = request.user
+    businesses = Business.get_business()
+    posts = Post.get_posts()
+
+    return render(request, 'index.html', {'current_user': current_user, 'businesses': businesses, 'posts': posts})
+
+
+def signup(request):
+    name = 'Signup'
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form, 'name': name})
+
+
+@login_required(login_url='/login/')
+def profile(request):
+    current_user = request.user
+
+    return render(request, 'profile.html', {'current_user': current_user, })
