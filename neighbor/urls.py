f3882c2b7a3cdbe8c5dad7b99021570009709abc
Collins Kanyiri <collins.kanyiri@student.moringaschool.com>
2021-11-02 10:37:22 +0300

    add paths

--- neighbor/urls.py
+++ neighbor/urls.py
@@ -5,7 +5,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-   
+    path('home', views.home, name='home'),
+    path('', views.signup, name='signup'),
+    path(r'login/', LoginView.as_view(), name='login'),
+    path(r'logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path(r'^profile/', views.profile, name='profile'),
+    path(r'^updateprofile/', views.update_profile, name='update_profile'),
+    path(r'^newbusiness/', views.new_business, name='new_business'),
+    path(r'^newpost/', views.new_post, name='new_post'),
+
 ]
 
 if settings.DEBUG:
