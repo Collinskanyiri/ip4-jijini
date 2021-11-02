04494530fa3c9cf40e70a991d3a4a5d506963b1c
Collins Kanyiri <collins.kanyiri@student.moringaschool.com>
2021-11-02 10:41:48 +0300

    add update profile,new business

--- neighbor/views.py
+++ neighbor/views.py
@@ -34,3 +34,52 @@ def profile(request):
     current_user = request.user
 
     return render(request, 'profile.html', {'current_user': current_user, })
+
+@login_required(login_url='/login/')
+def update_profile(request):
+    current_user = request.user
+    profile = Profile(user=request.user)
+   
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES,  instance=request.user.profile)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user= current_user
+            profile.save()
+        return redirect('home')
+    else:
+        form = ProfileForm(instance=request.user.profile)
+        args = {}
+        # args.update(csrf(request))
+        args['form'] = form
+    return render(request, 'update_profile.html', {'current_user':current_user, 'form':form})
+
+@login_required(login_url='/login/')
+def new_business(request):
+    current_user = request.user
+
+    if request.method == 'POST':
+        form = BusinessForm(request.POST, request.FILES)
+        if form.is_valid():
+            business = form.save(commit=False)
+            business.user= current_user
+            business.save()
+        return redirect('home')
+    else:
+        form = BusinessForm()
+    return render(request, 'business.html', {'current_user':current_user, 'form':form})
+
+@login_required(login_url='/login/')
+def new_post(request):
+    current_user = request.user
+   
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user= current_user
+            post.save()
+        return redirect('home')
+    else:
+        form = PostForm()
+    return render(request, 'new_post.html', {'current_user':current_user, 'form':form})
